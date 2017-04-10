bfe1b768296cb2a36947be69a3082469dd5e8766
gazaogomes <gazaogomes@hotmail.com>
2017-04-10 19:49:42 -0300

    Programa Calculadora Basica
    Merlin Calculadora

---
+++ calcBasic/src/calcbasic/CalcBasic.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcbasic;
+
+import javax.swing.JFrame;
+
+
+ //@author GabrielGomes
+ 
+public class CalcBasic {
+    float n1, n2, total;
+public CalcBasic(){
+    n1 = 0;
+    n2 = 0;
+    total =0;
+    }
+public void Somar(){
+total=n1+n2;
+}    
+public void Subtrair(){
+total=n1-n2;
+}  
+public void Dividir(){
+total=n1/n2;
+}  
+public void Multiplicar(){
+total=n1*n2;
+}  
+}
+ 
